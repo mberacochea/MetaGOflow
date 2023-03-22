121c57084477a4ac3bb197ed7e534f5e2b8a44f9
hariszaf <haris-zaf@hcmr.gr>
2023-03-23 01:56:23 +0200

    all test cases

--- slurm_run.sh
+++ slurm_run.sh
@@ -8,8 +8,8 @@
 #SBATCH --mail-user=haris.zafr@gmail.com
 #SBATCH --mail-type=ALL
 #SBATCH --requeue
-#SBATCH --job-name="rocr"
-#SBATCH --output=rocrates.output
+#SBATCH --job-name="rocr-tax"
+#SBATCH --output=rocrates_tax.output
 
 # Deactivate conda if already there
 conda deactivate
@@ -18,10 +18,23 @@ conda deactivate
 module load python/3.7.8
 module load singularity/3.7.1 
 
-# Run the wf
-./run_wf.sh -f test_input/wgs-paired-SRR1620013_1.fastq.gz -r test_input/wgs-paired-SRR1620013_2.fastq.gz -n dev_dataset -d DEV_DATASET -s
 
+# Run the wf with mini dataset
+./run_wf.sh -f test_input/wgs-paired-SRR1620013_1.fastq.gz -r test_input/wgs-paired-SRR1620013_2.fastq.gz -n mini_dataset -d MINI_DATASET -s
+
+# Run the wf with short dataset
+# ./run_wf.sh -f test_input/test_1_fwd_HWLTKDRXY_600000.fastq.gz -r test_input/test_2_rev_HWLTKDRXY_600000.fastq.gz -n dev_dataset -d DEV_DATASET -s
+
+# To run the manuscript use cases:
+# marine sediment
+# ./run_wf.sh -f test_input/DBH_AAAIOSDA_1_1_HMNJKDSX3.UDI224_clean.fastq.gz -r test_input/DBH_AAAIOSDA_1_2_HMNJKDSX3.UDI224_clean.fastq.gz -n DBH_dataset -d marine_sediment_dbh -s
+
+# column water
+# ./run_wf.sh -f test_input/DBB_AABVOSDA_1_1_HMNJKDSX3.UDI256_clean.fastq.gz -r test_input/DBB_AABVOSDA_1_2_HMNJKDSX3.UDI256_clean.fastq.gz -n DBB_dataset -d water_column_dbb -s
+
+# To run an ENA run
 # ./run_wf.sh -e ERR855786 -d TEST_SIMPLIFIED_PFAM -n ERR855786 -s
+
 
 # Disable the module
 module purge
